7a71ace0c17365c0447c9ff8a01a46c5a2fd6af4
Kireshanth <kireshantht@gmail.com>
2022-09-12 18:32:58 -0400

    removed lines of code that werent required

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
 const cors = require('cors');
 const connectDB = require('./config/database')
 const app = express()
@@ -10,12 +8,14 @@ require('dotenv').config({path: './config/.env'})
 
 app.use(cors());
 
+//run function to connect to mongoDB database
 connectDB()
 
-app.use(express.static('public'))
+//middleware to allow POST requests to recieve string or array data from form and JSON data
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+//
 app.use('/', categoryRoute)
 
 app.listen(PORT, ()=>{
